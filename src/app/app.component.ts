9ea5ab5bc6ff643437ae64f06b4a81d57e99c75a
= <=>
2018-07-10 23:52:31 +0200

    Actualizacao das paginas com o logo da seguradora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Indico Seguro Web Front-end Hello World';
 }
